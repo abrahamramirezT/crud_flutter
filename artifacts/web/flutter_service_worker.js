860c23a064cba164765e034c0b4498dd784644b8
abrahamramirezT <abrahamramirezT@users.noreply.github.com>
2024-08-02 20:49:57 +0000

    Commit the artifacts

--- artifacts/web/flutter_service_worker.js
+++ artifacts/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "264ed4ad80b40c998689476c888e49a0",
+const RESOURCES = {"flutter_bootstrap.js": "3b6ac25b98aaf09016ddc85e6144d448",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/AssetManifest.bin.json": "69a99f98c8b1fb8111c5fb961769fcd8",
 "assets/NOTICES": "29b4bf039b9ddeb7c00434d9cb5b1ddb",
@@ -31,7 +31,7 @@ const RESOURCES = {"flutter_bootstrap.js": "264ed4ad80b40c998689476c888e49a0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "b01cd5c1df54a64b20eff62045a21cae",
-"main.dart.js": "8c8878b4c917fe3a0c90b99ed3826d25",
+"main.dart.js": "9d29702ca67e78f717285e8ccb8a4f8b",
 "version.json": "2d98ae19f4c7d0386fc5572708296f41"};
 // The application shell files that are downloaded before a service worker can
 // start.
